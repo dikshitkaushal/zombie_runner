86bcd0b58751e00f06bbfbb946980616d999cf3b
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-04-09 11:32:24 +0530

    coin_logic

---
+++ 1_d-runner/Assets/scripts/coin_logic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin_logic : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip clip;
+    public MeshRenderer mesh;
+    private bool isplayed=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.right, 1f);
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+           
+            if (!isplayed)
+            {
+                isplayed = true;
+                source.PlayOneShot(clip);
+            }
+            mesh.enabled = false;
+           
+        }
+    }
+
+}
